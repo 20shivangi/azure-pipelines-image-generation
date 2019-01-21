9a957a3f2c8d33642cfe54c3b3aed5467ebe892e
David Staheli <dastahel@microsoft.com>
2019-01-21 09:11:02 -0500

    Android updates and Linux dynamic version output

--- images/win/scripts/Installers/Update-AndroidSDK.ps1
+++ images/win/scripts/Installers/Update-AndroidSDK.ps1
@@ -4,17 +4,17 @@
 ##  Desc:  Install and update Android SDK and tools
 ################################################################################
 
-# Get the latest command line tools so we can accept all of the licenses.  Alternatively we could just upload them.
-# https://developer.android.com/studio/index.html
-Invoke-WebRequest -UseBasicParsing -Uri "https://dl.google.com/android/repository/sdk-tools-windows-3859397.zip" -OutFile android-sdk-tools.zip
+# Download the latest command line tools so that we can accept all of the licenses.
+# See https://developer.android.com/studio/#command-tools
+Invoke-WebRequest -UseBasicParsing -Uri "https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip" -OutFile android-sdk-tools.zip
 
 # Don't replace the one that VS installs as it seems to break things.
 Expand-Archive -Path android-sdk-tools.zip -DestinationPath android-sdk -Force
 
 $sdk = Get-Item -Path .\android-sdk
 
-# Accept the standard licenses.  There does not appear to be an easy way to do this
-# so we are base64 encoding a zip of the lincenses directory from another installation
+# Accept the standard licenses.  There does not appear to be an easy way to do this,
+# so we are base64-encoding a zip of the licenses directory from another installation.
 $base64Content = "UEsDBBQAAAAIAPieZ0uamkPzKgAAACoAAAAhAAAAbGljZW5zZXNcYW5kcm9pZC1nb29nbGV0di1saWNlbnNlBcHRDQAwBAXA/ybdBQ9lHcL+I/Tunu1Un1gBLwZsWUv12s06RbtkEAB9UEsDBBQAAAAIAPmeZ0tHbeedKwAAACoAAAAcAAAAbGljZW5zZXNcYW5kcm9pZC1zZGstbGljZW5zZQXByQ0AIAwDsD8SuxDRi3FaaPYfAXuOp0ZFuPgRRWdxgeWRtMdb2EiTQH9QSwMEFAAAAAgA+p5nS5ECY7AsAAAAKgAAACQAAABsaWNlbnNlc1xhbmRyb2lkLXNkay1wcmV2aWV3LWxpY2Vuc2Xj5TI1MDEzM08zSTZISTVPTDNMNDBLSbVMM0kyNDcyT7IwMTY1TDOyNDQAAFBLAwQUAAAACAD7nmdLk6vQKCwAAAAqAAAAGwAAAGxpY2Vuc2VzXGdvb2dsZS1nZGstbGljZW5zZePlMjZOMks0SjIzMTMwTzM0TDI3tUwzNjJITbNMSUszSUw1TTYxT7E0TwQAUEsDBBQAAAAIAPyeZ0usTeMRLAAAACoAAAAkAAAAbGljZW5zZXNcaW50ZWwtYW5kcm9pZC1leHRyYS1saWNlbnNl4+VKsTQ3TTM3NTSztEg0N08yMzNKMzQyNUlJSUpNTTG2NDBMtTQ3SzNNBABQSwMEFAAAAAgA/Z5nS+2ee/8sAAAAKgAAACYAAABsaWNlbnNlc1xtaXBzLWFuZHJvaWQtc3lzaW1hZ2UtbGljZW5zZePlMjNOMTcwTjM1szRKS7GwNEwxTUxOTE5LSrFINbBMMzFIS7Y0NzM0MAUAUEsBAhQAFAAAAAgA+J5nS5qaQ/MqAAAAKgAAACEAAAAAAAAAAAAAAAAAAAAAAGxpY2Vuc2VzXGFuZHJvaWQtZ29vZ2xldHYtbGljZW5zZVBLAQIUABQAAAAIAPmeZ0tHbeedKwAAACoAAAAcAAAAAAAAAAAAAAAAAGkAAABsaWNlbnNlc1xhbmRyb2lkLXNkay1saWNlbnNlUEsBAhQAFAAAAAgA+p5nS5ECY7AsAAAAKgAAACQAAAAAAAAAAAAAAAAAzgAAAGxpY2Vuc2VzXGFuZHJvaWQtc2RrLXByZXZpZXctbGljZW5zZVBLAQIUABQAAAAIAPueZ0uTq9AoLAAAACoAAAAbAAAAAAAAAAAAAAAAADwBAABsaWNlbnNlc1xnb29nbGUtZ2RrLWxpY2Vuc2VQSwECFAAUAAAACAD8nmdLrE3jESwAAAAqAAAAJAAAAAAAAAAAAAAAAAChAQAAbGljZW5zZXNcaW50ZWwtYW5kcm9pZC1leHRyYS1saWNlbnNlUEsBAhQAFAAAAAgA/Z5nS+2ee/8sAAAAKgAAACYAAAAAAAAAAAAAAAAADwIAAGxpY2Vuc2VzXG1pcHMtYW5kcm9pZC1zeXNpbWFnZS1saWNlbnNlUEsFBgAAAAAGAAYA2gEAAH8CAAAAAA=="
 $content = [System.Convert]::FromBase64String($base64Content)
 Set-Content -Path .\android-sdk-licenses.zip -Value $content -Encoding Byte
@@ -26,7 +26,7 @@ Expand-Archive -Path .\android-sdk-licenses.zip -DestinationPath 'C:\Program Fil
 
 $sdk_root = "C:\Program Files (x86)\Android\android-sdk"
 
-#NDK is installed by VS
+# The NDK is installed by Visual Studio at this location:
 $ndk_root = "C:\Microsoft\AndroidNDK64\"
 
 if(Test-Path $ndk_root){
